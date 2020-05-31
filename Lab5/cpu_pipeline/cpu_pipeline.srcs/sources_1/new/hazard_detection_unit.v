47215108fb01873282ced1032bc20ca16cda00b4
Zhanghan Wang <895650485@qq.com>
2020-05-31 20:09:18 +0800

    lab5: a bad version of pipeline cpu

--- Lab5/cpu_pipeline/cpu_pipeline.srcs/sources_1/new/hazard_detection_unit.v
+++ Lab5/cpu_pipeline/cpu_pipeline.srcs/sources_1/new/hazard_detection_unit.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 2020/05/30 16:38:20
+// Create Date: 2020/05/30 16:40:10
 // Design Name: 
 // Module Name: hazard_detection_unit
 // Project Name: 
@@ -20,7 +20,26 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module hazard_detection_unit(
+module hazard_detection_unit
+(
+input mem_read,
+input [31:0] IF_ID_IR,
+input [31:0] ID_EX_IR,
+output reg pc_we,
+output reg stall
+);
 
-    );
+always @(*) begin
+    if(mem_read == 1'b1 
+        && (ID_EX_IR[20:16] == IF_ID_IR[25:21] 
+            || ID_EX_IR[20:16] == IF_ID_IR[20:16])) begin
+        pc_we = 1'b0;
+        stall = 1'b1;
+    end
+    else begin
+        pc_we = 1'b1;
+        stall = 1'b0;
+    end
+end    
+    
 endmodule
